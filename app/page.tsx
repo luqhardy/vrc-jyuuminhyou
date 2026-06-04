acd8f2362eedf6665b57a7d6c7e0aeee2b2fd65f
Luqman Hadi <hello@luqmanhadi.com>
2026-06-04 17:23:23 +0900

    br element remove

--- app/page.tsx
+++ app/page.tsx
@@ -291,13 +291,12 @@ export default function Home() {
           </p>
         </div>
       )}
-
+ 
       <div
         className={`mt-auto pt-6 w-full text-center text-[10px] relative z-10 h-40 animate-fade-in-up ${stage === "profile" ? "text-neutral-400" : "text-neutral-600"}`}
       >
         <p>
           © 2026 特定非営利活動法人 在日マレーシア人ふにゃおすなでなで推進委員会
-          <br />
           <br />
           <span className="opacity-80">
             (Specified Non-Profit Corporation: Malaysians in Japan Funyaosu
