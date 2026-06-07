aa515bbb00814df05e5756401ed245e478e9813c
Luqman Hadi <hello@luqmanhadi.com>
2026-06-08 02:44:48 +0900

    backrooms update

--- components/MotionBlur.tsx
+++ components/MotionBlur.tsx
@@ -1,4 +1,5 @@
-//"use client";
+"use client";
+import { useContext, useMemo } from "react";
 import { EffectComposerContext } from "@react-three/postprocessing";
 import { VelocityDepthNormalPass, MotionBlurEffect } from "realism-effects";
 import * as POSTPROCESSING from "postprocessing";
